bb0b163919ebd323a6da78dcd89b6606e26099c0
Spencer <snsallay@gmail.com>
2016-06-08 16:25:47 -0400

    change writing to binary

--- torpn.hs
+++ torpn.hs
@@ -1,19 +1,24 @@
 -- This program simply rewrites the program in RPN changing names to numeric codes and
 --   assigning an ID for each type.  Actual processing of the program happens in another program.
 
+import qualified Data.ByteString.Lazy as B (getContents,putStr)
+import qualified Data.ByteString.Char8.Lazy as BC (pack)
 import Text.Read (readMaybe)
+import Data.Binary (encode)
 
 main = do
   (x:_) <- getArgs
-  y <- readFile $ x ++ ".mug"
-  writeFile (x ++ ".muo") $ typify $ toRPN $ words y
+  y <- B.getContents $ x ++ ".mug"
+  writeFile (x ++ ".muo") $ out $ toRPN $ words y
 
 toRPN :: [String] -> [String]
 toRPN = reverse . filter (\x -> not $ elem x ["[","]"])
 
-typify = intersperse ' ' . concat . map (\x -> case readMaybe x :: Maybe Double of
-                                                 Nothing -> ["1", case findIndex (x==) f of Nothing -> (-1)
-                                                                                            Just a -> a]
-                                                 Just _  -> ["0", x])
+out :: [String] -> ByteString
+out = concat .
+  map (\x -> case readMaybe x :: Double of
+                 Nothing -> case findIndex (x==) f of Nothing -> (BC.pack "1")++encode (-1)::Int32
+                                                      Just a  -> (BC.pack "1")++encode a::Int32
+                 Just a -> (BC.pack "0")++encode a::Double)
 
 f = ["line","load","dup","save"]
