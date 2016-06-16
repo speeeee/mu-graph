78831db1c8a36a12b19f42656eb90d5c7c7cd464
Spencer <snsallay@gmail.com>
2016-06-16 17:20:29 -0400

    add test program (writes to out.muo)

---
+++ MGTest.hs
@@ -0,0 +1,13 @@
+import MuGraph
+import System.Random
+
+dat = ["T 2 8 8",
+       " 0 1 0 1 1 0 1 1 ",
+       " 1 0 0 0 1 1 0 0 ",
+       " 0 1 0 1 0 1 1 0 ",
+       " 1 1 0 1 1 0 1 1 ",
+       " 1 0 1 1 0 0 0 1 ",
+       " 1 0 0 0 0 0 0 1 ",
+       " 0 0 0 1 1 1 0 0 E"]
+
+main = do out_bc "out" $ concat dat
