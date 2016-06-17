70a4ad16952222b994074d4e881efd737c39788c
Spencer <snsallay@gmail.com>
2016-06-17 04:41:38 -0400

    switch to big-endian

--- MuGraph.hs
+++ MuGraph.hs
@@ -1,6 +1,7 @@
 module MuGraph (out_bc) where
 
-import qualified Data.ByteString.Lazy as B (ByteString(..),putStr,getContents,concat,append,writeFile)
+import qualified Data.ByteString.Lazy as B (ByteString(..),putStr,getContents,concat,append
+                                           ,writeFile,reverse)
 import qualified Data.ByteString.Lazy.Char8 as BC (pack)
 import Text.Read (readMaybe)
 import Data.Binary (encode)
@@ -17,7 +18,7 @@ toBC = B.concat .
                  Nothing -> case findIndex (x==) f of Nothing -> encode ((-1)::Int8)
                                                       Just a  -> encode (((fromIntegral a)+3)::Int8)
                  Just a -> case readMaybe x :: Maybe Int64 of
-                                Nothing -> B.append (encode (1::Int8)) $ encode (a::Double)
-                                Just a ->  B.append (encode (0::Int8)) $ encode (a::Int64))
+                                Nothing -> B.append (encode (1::Int8)) $ B.reverse $ encode (a::Double)
+                                Just a ->  B.append (encode (0::Int8)) $ B.reverse $ encode (a::Int64))
 
 f = ["T","E"]
